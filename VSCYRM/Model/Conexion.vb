b3fc7a98675a66b0cbf7cc52db60e3b8b7ca3271
Marvic Roman Santiago Perea <ciaomarvic@gmail.com>
2016-10-09 14:09:02 -0400

    Correcion IDisposable

--- VSCYRM/Model/Conexion.vb
+++ VSCYRM/Model/Conexion.vb
@@ -1,28 +1,54 @@
 ﻿Imports MySql.Data.MySqlClient
-Public Class Conexion
-    Public Connection As MySqlConnection
-    Public Command As MySqlCommand
-    Public Sub ConectarMysql()
-        Try
-            Connection = New MySqlConnection()
-            Connection.ConnectionString = "server=127.0.0.1;" _
+Imports System
+
+Namespace DesignMysql
+    Public Class Conexion
+        Implements IDisposable
+        Public Connection As MySqlConnection
+        Public Command As MySqlCommand
+
+
+        Public Sub ConectarMysql()
+
+            Try
+                Connection = New MySqlConnection()
+                Connection.ConnectionString = "server=127.0.0.1;" _
             & "uid=root;" _
             & "pwd=s3rv3r;" _
             & "port=3306;" _
             & "database=vscyrm;"
 
-            Connection.Open()
-            '  MsgBox("Conectado " &
-            '  vbCrLf & vbCrLf,
-            '  MsgBoxStyle.OkOnly + MsgBoxStyle.Information)
-        Catch ex As Exception
-            MsgBox("Error al conectar al servidor MySQL " &
+                Connection.Open()
+                '  MsgBox("Conectado " &
+                '  vbCrLf & vbCrLf,
+                '  MsgBoxStyle.OkOnly + MsgBoxStyle.Information)
+            Catch ex As Exception
+                MsgBox("Error al conectar al servidor MySQL " &
             vbCrLf & vbCrLf & ex.Message,
             MsgBoxStyle.OkOnly + MsgBoxStyle.Critical)
-        End Try
-    End Sub
-    Public Sub DesconectarMysql()
-        Connection.Close()
-    End Sub
+            End Try
 
-End Class
+        End Sub
+        Public Sub DesconectarMysql()
+            Connection.Close()
+        End Sub
+        Protected Overridable Overloads Sub Dispose(disposing As Boolean)
+
+            If disposing Then
+                ' dispose managed resources
+                Connection.Close()
+            End If
+        End Sub 'Dispose
+
+
+        Public Overloads Sub Dispose() Implements IDisposable.Dispose
+
+            Dispose(True)
+            GC.SuppressFinalize(Me)
+
+        End Sub 'Dispose
+
+
+    End Class
+End Namespace
+
